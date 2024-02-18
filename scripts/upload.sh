b4107b4d57e1360e784b9510351764f3d53a859f
Cuma.Rull <102359107+cumaRull@users.noreply.github.com>
2024-02-18 22:59:36 +0700

    .

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-  ZIP1=${GITHUB_WORKSPACE}/android/AnyKernel/${{ github.event.inputs.ZIP_NAME }}-${{ github.event.inputs.KERNEL_DEFCONFIG }}-${{ env.BUILD_DATE }}.zip
+  sudo cd ${GITHUB_WORKSPACE}/android/AnyKernel/
+  ZIP1=${{ github.event.inputs.ZIP_NAME }}-${{ github.event.inputs.KERNEL_DEFCONFIG }}-${{ env.BUILD_DATE }}.zip
   curl -F document=@$ZIP1 "https://api.telegram.org/bot${{ secrets.TG_BOT_TOKEN }}/sendDocument" \
     -F chat_id="${{ secrets.TG_CHAT_ID }}" \
     -F "disable_web_page_preview=true" \
