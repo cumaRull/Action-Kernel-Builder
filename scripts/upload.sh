3384f57390ad25eb02c18f01046bae0d1509bd8e
Cuma.Rull <102359107+cumaRull@users.noreply.github.com>
2024-02-18 21:09:52 +0700

    .

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 push() {
-  ZIP1=$(echo ${NAME_KERNEL}-*.zip)
-  curl -F document=@$ZIP1 "https://api.telegram.org/bot$token/sendDocument" \
-    -F chat_id="$chat_id" \
+  ZIP1=${GITHUB_WORKSPACE}/android/AnyKernel/${{ github.event.inputs.ZIP_NAME }}-${{ github.event.inputs.KERNEL_DEFCONFIG }}-${{ env.BUILD_DATE }}.zip
+  curl -F document=@$ZIP1 "https://api.telegram.org/bot${{ secrets.TG_BOT_TOKEN }}/sendDocument" \
+    -F chat_id="${{ secrets.TG_CHAT_ID }}" \
     -F "disable_web_page_preview=true" \
     -F "parse_mode=html" \
     -F caption="Build finished and pushing in Telegram"
