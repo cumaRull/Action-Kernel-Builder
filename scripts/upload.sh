fc3a05c3f9721c2069f17965c4769b5cc8279434
Cuma.Rull <102359107+cumaRull@users.noreply.github.com>
2024-02-24 19:20:14 +0700

    .

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ZIP1=${{ github.event.inputs.ZIP_NAME }}-${{ github.event.inputs.KERNEL_DEFCONFIG }}-${{ env.BUILD_DATE }}.zip
+ZIP1=Image.gz-dtb
 curl -F document=@$ZIP1 "https://api.telegram.org/bot${{ secrets.TG_BOT_TOKEN }}/sendDocument" \
     -F chat_id="${{ secrets.TG_CHAT_ID }}" \
     -F "disable_web_page_preview=true" \
