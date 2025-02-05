b1b4d588a04085e391e605699495aee04ab8dc35
cumaRull <rulsmods@gmail.com>
2025-02-05 16:10:09 +0700

    builder: build with gcc
    Signed-off-by: cumaRull <rulsmods@gmail.com>

--- scripts/upload.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ZIP1=Image.gz-dtb
-curl -F document=@$ZIP1 "https://api.telegram.org/bot${{ secrets.TG_BOT_TOKEN }}/sendDocument" \
-    -F chat_id="${{ secrets.TG_CHAT_ID }}" \
-    -F "disable_web_page_preview=true" \
-    -F "parse_mode=html" \
-    -F caption="Build finished and pushing in Telegram"
